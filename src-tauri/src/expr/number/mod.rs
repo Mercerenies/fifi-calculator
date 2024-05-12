74252970f0f7c2eebfe55aa2ec1fafb5ccbb21c0
Silvio Mayolo <mercerenies@comcast.net>
2024-05-11 22:00:06 -0400

    Number parsing infrastructure

--- src-tauri/src/expr/number/mod.rs
+++ src-tauri/src/expr/number/mod.rs
@@ -4,9 +4,13 @@ mod visitor;
 use visitor::NumberPair;
 
 use num::{BigInt, BigRational, Zero, ToPrimitive, One};
+use thiserror::Error;
+use once_cell::sync::Lazy;
+use regex::Regex;
 
 use std::fmt::{self, Display, Formatter};
 use std::ops::{Add, Sub, Mul, Div};
+use std::str::FromStr;
 
 /// General-purpose number type, capable of automatically switching
 /// between representations when mathematical functions demand it.
@@ -21,6 +25,10 @@ enum NumberImpl {
   Ratio(BigRational),
   Float(f64),
 }
+
+#[derive(Error, Debug)]
+#[non_exhaustive]
+pub struct ParseNumberError {}
 
 /// The different ways a number can be represented. These are ordered
 /// in terms of priority, so if `a <= b`, that implies that the
@@ -51,6 +59,16 @@ impl Number {
       NumberImpl::Float(_) => NumberRepr::Float,
     }
   }
+
+  pub fn strict_eq(&self, other: &Number) -> bool {
+    self.repr() == other.repr() && self == other
+  }
+
+  /// Produces a rational number with repr [`NumberRepr::Ratio`].
+  /// Panics if `denom == 0`.
+  pub fn ratio(numer: BigInt, denom: BigInt) -> Number {
+    Number::from(BigRational::new(numer, denom))
+  }
 }
 
 impl From<i64> for Number {
@@ -80,9 +98,40 @@ impl From<f64> for Number {
 impl Display for Number {
   fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
     match &self.inner {
-      NumberImpl::Integer(i) => i.fmt(f),
-      NumberImpl::Ratio(r) => r.fmt(f),
-      NumberImpl::Float(d) => d.fmt(f),
+      NumberImpl::Integer(i) => {
+        i.fmt(f)
+      }
+      NumberImpl::Ratio(r) => {
+        write!(f, "{}:{}", r.numer(), r.denom())
+      }
+      NumberImpl::Float(d) => {
+        // If the float is actually a (small) integer, force one decimal
+        // point. Otherwise, use default printer.
+        if d.fract().is_zero() && d.abs() < u64::MAX as f64 {
+          write!(f, "{:.1}", d)
+        } else {
+          write!(f, "{}", d)
+        }
+      }
+    }
+  }
+}
+
+impl Display for ParseNumberError {
+  fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+    write!(f, "Failed to parse number")
+  }
+}
+
+/// `PartialEq` impl for `Number` compares the numerical value and
+/// ignores the representation. To include the representation, use
+/// [`Number::strict_eq`].
+impl PartialEq for Number {
+  fn eq(&self, other: &Number) -> bool {
+    match NumberPair::promote(self.clone(), other.clone()) {
+      NumberPair::Integers(left, right) => left == right,
+      NumberPair::Ratios(left, right) => left == right,
+      NumberPair::Floats(left, right) => left == right,
     }
   }
 }
@@ -190,5 +239,102 @@ impl One for Number {
       NumberImpl::Ratio(r) => r.is_one(),
       NumberImpl::Float(f) => f.is_one(),
     }
+  }
+}
+
+impl FromStr for Number {
+  type Err = ParseNumberError;
+
+  fn from_str(s: &str) -> Result<Number, ParseNumberError> {
+    parse_integer(s).or_else(|| {
+      parse_ratio(s)
+    }).or_else(|| {
+      parse_float(s)
+    }).ok_or(ParseNumberError {})
+  }
+}
+
+fn parse_integer(s: &str) -> Option<Number> {
+  BigInt::from_str(s).map(Number::from).ok()
+}
+
+fn parse_ratio(s: &str) -> Option<Number> {
+  static RE: Lazy<Regex> = Lazy::new(|| Regex::new(r"([+-]?\d+):([+-]?\d+)").unwrap());
+  let Some(caps) = RE.captures(s) else { return None; };
+  // Note: We panic if BigInt::from_str fails here, since the regex
+  // *should* guarantee it succeeds. If that assumption fails, it's a
+  // bug and I want to know.
+  let numerator = BigInt::from_str(caps.get(1).unwrap().as_str()).unwrap();
+  let denominator = BigInt::from_str(caps.get(2).unwrap().as_str()).unwrap();
+  if denominator.is_zero() {
+    // Fail the parse.
+    return None;
+  }
+  let ratio = BigRational::new(numerator, denominator);
+  Some(Number::from(ratio))
+}
+
+fn parse_float(s: &str) -> Option<Number> {
+  f64::from_str(s).map(Number::from).ok()
+}
+
+#[cfg(test)]
+mod tests {
+  use super::*;
+  use num::bigint::Sign;
+
+  // TODO Incomplete test suite in this file :)
+
+  fn roundtrip_display(number: Number) -> Number {
+    Number::from_str(&number.to_string()).unwrap()
+  }
+
+  fn assert_roundtrip_display(number: Number) {
+    assert!(number.strict_eq(&roundtrip_display(number.clone())));
+  }
+
+  #[test]
+  fn test_display_roundtrip() {
+    // Small integers
+    assert_roundtrip_display(Number::from(0i64));
+    assert_roundtrip_display(Number::from(10i64));
+    assert_roundtrip_display(Number::from(999i64));
+    assert_roundtrip_display(Number::from(-99i64));
+    // Big integers
+    assert_roundtrip_display(Number::from(BigInt::from_slice(Sign::Plus, &[9, 10, 100, 488, 22, 3])));
+    assert_roundtrip_display(Number::from(BigInt::from_slice(Sign::Minus, &[9, 100, 488, 10, 22, 3])));
+    // Rational numbers
+    assert_roundtrip_display(Number::ratio(BigInt::from(9), BigInt::from(100)));
+    assert_roundtrip_display(Number::ratio(BigInt::from(-100), BigInt::from(3)));
+    assert_roundtrip_display(Number::ratio(BigInt::from(38324), BigInt::from(288)));
+    // Floats
+    assert_roundtrip_display(Number::from(9.1));
+    assert_roundtrip_display(Number::from(3.1415));
+    assert_roundtrip_display(Number::from(93763782432.22));
+    assert_roundtrip_display(Number::from(-9.000001));
+    assert_roundtrip_display(Number::from(-8.0));
+  }
+
+  #[test]
+  fn test_parse_integer() {
+    assert!(Number::from_str("7").unwrap().strict_eq(&Number::from(7i64)));
+    assert!(Number::from_str("-99").unwrap().strict_eq(&Number::from(-99i64)));
+    assert!(Number::from_str("888888888888888888888888888888888").unwrap().strict_eq(&Number::from(BigInt::from_str("888888888888888888888888888888888").unwrap())));
+  }
+
+  #[test]
+  fn test_parse_ratio() {
+    assert!(Number::from_str("1:2").unwrap().strict_eq(&Number::ratio(BigInt::from(1), BigInt::from(2))));
+    assert!(Number::from_str("-7:9").unwrap().strict_eq(&Number::ratio(BigInt::from(-7), BigInt::from(9))));
+    assert!(Number::from_str("7:-9").unwrap().strict_eq(&Number::ratio(BigInt::from(-7), BigInt::from(9))));
+    assert!(Number::from_str("1:0").is_err());
+  }
+
+  #[test]
+  fn test_parse_float() {
+    assert!(Number::from_str("1.9").unwrap().strict_eq(&Number::from(1.9f64)));
+    assert!(Number::from_str("-88.0").unwrap().strict_eq(&Number::from(-88f64)));
+    assert!(Number::from_str("3e-6").unwrap().strict_eq(&Number::from(3e-6f64)));
+    assert!(Number::from_str("3e6").unwrap().strict_eq(&Number::from(3e6f64)));
   }
 }
