3232eb82d24e1cf7ef37ea96f502628018b4070e
Silvio Mayolo <sdmayolo42@students.tntech.edu>
2024-05-16 23:20:01 -0400

    typechecker on vec

--- src-tauri/src/expr/simplifier/evaluator/typechecker.rs
+++ src-tauri/src/expr/simplifier/evaluator/typechecker.rs
@@ -36,9 +36,45 @@ pub struct IsNumber {
   _private: (),
 }
 
+/// Lift a type-checker into each element of a `Vec`.
+#[derive(Debug, Clone, Default)]
+pub struct OnVec<C, T> {
+  inner: C,
+  _phantom: PhantomData<T>,
+}
+
 impl<I> Identity<I> {
   pub fn new() -> Self {
     Self::default()
+  }
+}
+
+impl IsNumber {
+  pub fn new() -> Self {
+    Self::default()
+  }
+}
+
+impl<C, T> OnVec<C, T>
+where C: TypeChecker<T> {
+  pub fn new(inner: C) -> Self {
+    Self { inner, _phantom: PhantomData }
+  }
+
+  fn recover_failed_downcast<I>(
+    &self,
+    some_outputs: Vec<C::Output>,
+    current_element: T,
+    rest_of_inputs: I,
+  ) -> Vec<T>
+  where I: Iterator<Item=T> {
+    let mut inputs = Vec::with_capacity(some_outputs.len() * 2);
+    for out in some_outputs {
+      inputs.push(self.inner.widen_type(out));
+    }
+    inputs.push(current_element);
+    inputs.extend(rest_of_inputs);
+    inputs
   }
 }
 
@@ -60,12 +96,6 @@ impl<I> TypeChecker<I> for Identity<I> {
   }
 }
 
-impl IsNumber {
-  pub fn new() -> Self {
-    Self::default()
-  }
-}
-
 impl TypeChecker<Expr> for IsNumber {
   type Output = Number;
 
@@ -75,5 +105,28 @@ impl TypeChecker<Expr> for IsNumber {
 
   fn widen_type(&self, value: Number) -> Expr {
     Expr::from(value)
+  }
+}
+
+impl<C, T> TypeChecker<Vec<T>> for OnVec<C, T>
+where C: TypeChecker<T> {
+  type Output = Vec<C::Output>;
+
+  fn narrow_type(&self, input: Vec<T>) -> Result<Vec<C::Output>, Vec<T>> {
+    let mut output = Vec::with_capacity(input.len());
+    let mut iter = input.into_iter();
+    while let Some(elem) = iter.next() {
+      match self.inner.narrow_type(elem) {
+        Ok(elem) => output.push(elem),
+        Err(elem) => {
+          return Err(self.recover_failed_downcast(output, elem, iter));
+        }
+      }
+    }
+    Ok(output)
+  }
+
+  fn widen_type(&self, value: Vec<C::Output>) -> Vec<T> {
+    value.into_iter().map(|i| self.inner.widen_type(i)).collect()
   }
 }
