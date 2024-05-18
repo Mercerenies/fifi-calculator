ef3c93d86268552d57465103774d68389a6ca593
Silvio Mayolo <sdmayolo42@students.tntech.edu>
2024-05-17 23:16:52 -0400

    more prism docs

--- src-tauri/src/util/prism.rs
+++ src-tauri/src/util/prism.rs
@@ -7,6 +7,24 @@ use std::marker::PhantomData;
 /// relationship between `Up` and `Down`. Specifically, it asserts
 /// that every `Down` can be seen as an `Up` in a well-defined way,
 /// and that some `Up`s can be safely downcast to type `Down`.
+///
+/// This prism implementation is based loosely on [the Haskell lens
+/// library](https://hackage.haskell.org/package/lens-5.3.2/docs/Control-Lens-Prism.html),
+/// and prisms implementing this trait should satisfy similar laws.
+///
+/// * A widen followed by a narrow should reproduce the original
+/// value. That is, for all `d: Down`,
+/// `prism.narrow_type(prism.widen_type(d)) === Some(d)`.
+///
+/// * A successful narrow, followed by a widen, should reproduce the
+/// original value completely. That is, for all `u: Up`, if
+/// `prism.narrow_type(u) = Ok(d)`, then `prism.widen_type(d) === u`.
+///
+/// * A failed narrow shall return the original value. That is, for
+/// all `u: Up`, if `prism.narrow_type(u) = Err(u1)`, then `u === u1`.
+///
+/// where `===` is taken to mean "conceptually equal". You can think
+/// of this as `==` for types that implement `PartialEq`.
 pub trait Prism<Up, Down> {
   /// Attempts to downcast `input` to the type `Down`. This method
   /// shall either return the result of successfully downcasting (as
