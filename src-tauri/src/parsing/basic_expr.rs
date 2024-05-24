420355b3155645c2ff85e9e750c525e224373648
Silvio Mayolo <sdmayolo42@students.tntech.edu>
2024-05-24 14:16:23 -0400

    Add spans to basic_expr

--- src-tauri/src/parsing/basic_expr.rs
+++ src-tauri/src/parsing/basic_expr.rs
@@ -1,16 +1,17 @@
 
 use crate::expr::number::Number;
+use super::source::Span;
 
 /// A basic expression type, without any operator precedence
 /// semantics.
 #[derive(Clone, Debug)]
 pub enum BasicExpr {
   Scalar(Number),
-  OperatorChain(Box<BasicExpr>, Vec<OperatorApp>),
+  OperatorChain(Box<(BasicExpr, Span)>, Vec<OperatorApp>),
 }
 
 #[derive(Clone, Debug)]
 pub struct OperatorApp {
-  pub operator: String,
-  pub right_hand_side: BasicExpr,
+  pub operator: (String, Span),
+  pub right: (BasicExpr, Span),
 }
