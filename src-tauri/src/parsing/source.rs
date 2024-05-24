0b93f61bc7068baa598e7c46fa7a3f01e812f2d0
Silvio Mayolo <sdmayolo42@students.tntech.edu>
2024-05-24 11:48:59 -0400

    helpers for source

--- src-tauri/src/parsing/source.rs
+++ src-tauri/src/parsing/source.rs
@@ -1,9 +1,13 @@
 
+use std::fmt::{self, Display, Formatter};
+
 /// Thin wrapper around `usize` that represents a position in a parsed
 /// string. Usually used for error reporting.
 #[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord)]
 pub struct SourceOffset(pub usize);
 
+/// A span of source offsets. Spans should be considered half-open
+/// intervals, with `start` being included and `end` being excluded.
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 pub struct Span {
   pub start: SourceOffset,
@@ -25,5 +29,17 @@ impl From<usize> for SourceOffset {
 impl From<SourceOffset> for usize {
   fn from(i: SourceOffset) -> Self {
     i.0
+  }
+}
+
+impl Display for SourceOffset {
+  fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+    write!(f, "{}", self.0)
+  }
+}
+
+impl Display for Span {
+  fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+    write!(f, "{}-{}", self.start, self.end)
   }
 }
