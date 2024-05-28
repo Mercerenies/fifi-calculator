45bc650db411a73d8ee2a1c8aac37689b58cf5b2
Silvio Mayolo <mercerenies@comcast.net>
2024-05-28 15:02:48 -0400

    custom_error

--- src-tauri/src/error.rs
+++ src-tauri/src/error.rs
@@ -5,9 +5,13 @@ use crate::expr::number::ParseNumberError;
 
 use thiserror::Error;
 
+use std::error::{Error as StdError};
+
 #[derive(Debug, Error)]
 #[non_exhaustive]
 pub enum Error {
+  #[error("{0}")]
+  CustomError(Box<dyn StdError + Send + Sync + 'static>),
   #[error("{0}")]
   TauriError(#[from] tauri::Error),
   #[error("{0}")]
@@ -16,4 +20,10 @@ pub enum Error {
   NoSuchCommandError(#[from] NoSuchCommandError),
   #[error("{0}")]
   ParseNumberError(#[from] ParseNumberError),
+}
+
+impl Error {
+  pub fn custom_error(err: impl StdError + Send + Sync + 'static) -> Self {
+    Self::CustomError(Box::new(err))
+  }
 }
