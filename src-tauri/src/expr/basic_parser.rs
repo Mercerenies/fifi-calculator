fe743a4d6aab20ecb03e561a791f7bc3305673ac
Silvio Mayolo <mercerenies@comcast.net>
2024-05-28 14:13:28 -0400

    parser

--- src-tauri/src/expr/basic_parser.rs
+++ src-tauri/src/expr/basic_parser.rs
@@ -3,15 +3,47 @@
 //! mode](crate::display::basic::BasicLanguageMode].
 
 use super::Expr;
-use super::tokenizer::ExprTokenizer;
-use crate::parsing::shunting_yard::ShuntingYardDriver;
+use super::number::ComplexNumber;
+use super::tokenizer::{ExprTokenizer, Token, TokenData, TokenizerError};
+use crate::parsing::shunting_yard::{self, ShuntingYardDriver, ShuntingYardError};
 use crate::parsing::operator::{Operator, OperatorTable};
+use crate::parsing::source::{Span, SourceOffset};
+use crate::parsing::tokenizer::TokenizerState;
+
+use thiserror::Error;
 
 use std::convert::Infallible;
 
 #[derive(Clone, Debug)]
 pub struct ExprParser<'a> {
   tokenizer: ExprTokenizer<'a>,
+}
+
+#[derive(Clone, Debug, Error)]
+pub enum ParseError {
+  #[error("Tokenizer error: {0}")]
+  TokenizerError(#[from] TokenizerError),
+  #[error("Parsing error: {0}")]
+  ParsingError(#[from] ParsingError),
+  #[error("Operator precedence error: {0}")]
+  ShuntingYardError(#[from] ShuntingYardError<Expr, Infallible>),
+}
+
+#[derive(Clone, Debug, Error)]
+#[non_exhaustive]
+pub enum ParsingError {
+  #[error("Expected start of expression at {0}")]
+  ExpectedStartOfExpr(SourceOffset),
+  #[error("Expected right parenthesis or comma, got {0} at {1}")]
+  ExpectedRParenOrComma(TokenData, SourceOffset),
+  #[error("Expected parenthesized expression or complex number at {0}")]
+  ExpectedParensOrComplex(SourceOffset),
+  #[error("Expected operator")]
+  ExpectedOperator,
+  #[error("Unexpected EOF")]
+  UnexpectedEOF, // TODO: We don't get SourceOffset here; find a way to get that info
+  #[error("Expecting EOF at {0}")]
+  ExpectedEOF(SourceOffset),
 }
 
 #[derive(Clone, Debug, Default)]
@@ -27,6 +59,145 @@ impl<'a> ExprParser<'a> {
 
   pub fn tokenizer(&self) -> &ExprTokenizer<'a> {
     &self.tokenizer
+  }
+
+  pub fn tokenize_and_parse(&self, input: &str) -> Result<Expr, ParseError> {
+    let mut state = TokenizerState::new(input);
+    let tokens = self.tokenizer.read_tokens(&mut state)?;
+    let expr = self.parse(&tokens)?;
+    Ok(expr)
+  }
+
+  pub fn parse(&self, stream: &[Token]) -> Result<Expr, ParseError> {
+    let (expr, stream) = self.parse_expr(stream)?;
+    match stream.first() {
+      Some(stray_token) => Err(ParsingError::ExpectedEOF(stray_token.span.start).into()),
+      None => Ok(expr),
+    }
+  }
+
+  fn parse_expr<'t>(&self, stream: &'t [Token]) -> Result<(Expr, &'t [Token]), ParseError> {
+    let Some(token) = stream.first() else {
+      return Err(ParsingError::UnexpectedEOF.into());
+    };
+    match &token.data {
+      TokenData::Operator(_) | TokenData::Comma | TokenData::RightParen => {
+        // Note: If we ever write any prefix operators, then
+        // TokenData::Operator needs to start an operator chain, not
+        // be an error. Right now we only have infix operators, so
+        // it's a parse error.
+        Err(ParsingError::ExpectedStartOfExpr(token.span.start).into())
+      }
+      TokenData::FunctionCallStart(_) | TokenData::LeftParen | TokenData::Number(_) => {
+        self.parse_operator_chain(stream)
+      }
+    }
+  }
+
+  fn parse_operator_chain<'t>(&self, mut stream: &'t [Token]) -> Result<(Expr, &'t [Token]), ParseError> {
+    let mut tokens: Vec<shunting_yard::Token<Expr>> = Vec::new();
+    // First expression.
+    let ((expr, span), tail) = self.parse_atom(stream)?;
+    stream = tail;
+    tokens.push(shunting_yard::Token::scalar(expr, span));
+    // Rest of operator-expression sequences.
+    while let Ok(((op, span), tail)) = self.parse_operator(stream) {
+      tokens.push(shunting_yard::Token::operator(op, span));
+      stream = tail;
+      let ((expr, span), tail) = self.parse_atom(stream)?;
+      stream = tail;
+      tokens.push(shunting_yard::Token::scalar(expr, span));
+    }
+    // Now use shunting yard to simplify the vector.
+    let mut shunting_yard_driver = ExprShuntingYardDriver::new();
+    let expr = shunting_yard::parse(&mut shunting_yard_driver, tokens)?;
+    Ok((expr, stream))
+  }
+
+  fn parse_operator<'t>(&self, stream: &'t [Token]) -> Result<((Operator, Span), &'t [Token]), ParseError> {
+    if let Some(Token { data: TokenData::Operator(op), span }) = stream.first() {
+      Ok(((op.clone(), *span), &stream[1..]))
+    } else {
+      Err(ParsingError::ExpectedOperator.into())
+    }
+  }
+
+  fn parse_atom<'t>(&self, stream: &'t [Token]) -> Result<((Expr, Span), &'t [Token]), ParseError> {
+    let Some(token) = stream.first() else {
+      return Err(ParsingError::UnexpectedEOF.into());
+    };
+    match &token.data {
+      TokenData::Number(n) => {
+        Ok(((Expr::from(n.clone()), token.span), &stream[1..]))
+      }
+      TokenData::FunctionCallStart(f) => {
+        let ((args, end), tail) = self.parse_function_args(&stream[1..])?;
+        Ok(((Expr::call(f, args), Span::new(token.span.start, end)), tail))
+      }
+      TokenData::LeftParen => {
+        let ((mut args, end), tail) = self.parse_function_args(&stream[1..])?;
+        let span = Span::new(token.span.start, end);
+        match args.len() {
+          1 => {
+            // Parenthesized expression, just return the inside
+            let arg = args.pop().unwrap();
+            Ok(((arg, span), tail))
+          }
+          2 => {
+            // Complex number expression. Right now we write this
+            // explicitly and count on a simplifier to simplify it if
+            // given literal numbers.
+            let imag = args.pop().unwrap();
+            let real = args.pop().unwrap();
+            let expr = Expr::call("+", vec![real, Expr::call("*", vec![imag, Expr::from(ComplexNumber::ii())])]);
+            Ok(((expr, span), tail))
+          }
+          _ => {
+            Err(ParsingError::ExpectedParensOrComplex(token.span.start).into())
+          }
+        }
+      }
+      _ => {
+        Err(ParsingError::ExpectedStartOfExpr(token.span.start).into())
+      }
+    }
+  }
+
+  // Expects and consumes a closing parenthesis at the end.
+  fn parse_function_args<'t>(&self, mut stream: &'t [Token]) -> Result<((Vec<Expr>, SourceOffset), &'t [Token]), ParseError> {
+    let close_paren_offset: SourceOffset;
+    let mut output = Vec::new();
+    loop {
+      let Some(token) = stream.first() else {
+        return Err(ParsingError::UnexpectedEOF.into());
+      };
+      if token.data == TokenData::RightParen {
+        close_paren_offset = token.span.end;
+        stream = &stream[1..];
+        break;
+      }
+      let (next_expr, tail) = self.parse_expr(stream)?;
+      output.push(next_expr);
+      stream = tail;
+      match stream.first() {
+        Some(Token { data: TokenData::Comma, .. }) => {
+          // Consume the comma (might be a trailing comma, but we
+          // allow those).
+          stream = &stream[1..];
+        }
+        Some(Token { data: TokenData::RightParen, .. }) => {
+          // We're at the end of the list; next loop iteration will
+          // terminate. Do nothing for now.
+        }
+        None => {
+          return Err(ParsingError::UnexpectedEOF.into());
+        }
+        Some(Token { data, span }) => {
+          return Err(ParsingError::ExpectedRParenOrComma(data.clone(), span.start).into());
+        }
+      }
+    }
+    Ok(((output, close_paren_offset), stream))
   }
 }
 
@@ -46,5 +217,26 @@ impl ShuntingYardDriver<Expr> for ExprShuntingYardDriver {
 
   fn compile_bin_op(&mut self, left: Expr, oper: Operator, right: Expr) -> Result<Expr, Infallible> {
     Ok(Expr::call(oper.name(), vec![left, right]))
+  }
+}
+
+#[cfg(test)]
+mod tests {
+  use super::*;
+  use crate::expr::number::Number;
+
+  #[test]
+  fn test_atom_parse() {
+    let table = OperatorTable::common_operators();
+    let parser = ExprParser::new(&table);
+
+    let expr = parser.tokenize_and_parse("1").unwrap();
+    assert_eq!(expr, Expr::from(1));
+
+    let expr = parser.tokenize_and_parse("1.5").unwrap();
+    assert_eq!(expr, Expr::from(Number::from(1.5)));
+
+    let expr = parser.tokenize_and_parse("-1:3").unwrap();
+    assert_eq!(expr, Expr::from(Number::ratio(-1, 3)));
   }
 }
