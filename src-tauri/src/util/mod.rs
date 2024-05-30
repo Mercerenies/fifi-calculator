d47008da6ee6e14a0b9f350d3679812ce15e370b
Silvio Mayolo <mercerenies@comcast.net>
2024-05-30 12:40:00 -0400

    helper functions

--- src-tauri/src/util/mod.rs
+++ src-tauri/src/util/mod.rs
@@ -46,6 +46,38 @@ pub fn clamp<T: PartialOrd>(val: T, min: T, max: T) -> T {
   if val < min { min } else if val > max { max } else { val }
 }
 
+/// Returns a count of the number of elements at the beginning of this
+/// iterator which satisfy the predicate.
+pub fn count_prefix<I, F>(iter: I, mut predicate: F) -> usize
+where F: FnMut(I::Item) -> bool,
+      I: Iterator {
+  let mut count = 0;
+  for item in iter {
+    if predicate(item) {
+      count += 1;
+    } else {
+      break;
+    }
+  }
+  count
+}
+
+/// Returns a count of the number of elements at the end of this
+/// (double-ended) iterator which satisfy the predicate.
+pub fn count_suffix<I, F>(iter: I, mut predicate: F) -> usize
+where F: FnMut(I::Item) -> bool,
+      I: Iterator + DoubleEndedIterator {
+  let mut count = 0;
+  for item in iter.rev() {
+    if predicate(item) {
+      count += 1;
+    } else {
+      break;
+    }
+  }
+  count
+}
+
 #[cfg(test)]
 mod tests {
   use super::*;
@@ -87,5 +119,31 @@ mod tests {
   #[test]
   fn test_regex_opt_with_output() {
     assert_eq!(regex_opt_with(["**", "(x"], |s| format!("^{s}")).to_string(), r"^(?:\*\*|\(x)");
+  }
+
+  #[test]
+  fn test_count_prefix() {
+    let list = vec![0, 1, 2, 3, 4, 5, 6, 5, 4, 3, 2, 1, 0, 1, 2, 3, 4, 5, 6];
+    assert_eq!(count_prefix(list.into_iter(), |x| x < 4), 4);
+  }
+
+  #[test]
+  fn test_count_suffix() {
+    let list = vec![0, 1, 2, 3, 4, 5, 6, 5, 4, 3, 2, 1, 0, 1, 2, 3, 4, 5, 6];
+    assert_eq!(count_suffix(list.into_iter(), |x| x > 2), 4);
+  }
+
+  #[test]
+  fn test_count_prefix_suffix_whole_list() {
+    let list = vec![0, 1, 2, 3, 10, 20, 30];
+    assert_eq!(count_prefix(list.iter(), |_| true), 7);
+    assert_eq!(count_suffix(list.iter(), |_| true), 7);
+  }
+
+  #[test]
+  fn test_count_prefix_suffix_none_of_list() {
+    let list = vec![0, 1, 2, 3, 10, 20, 30];
+    assert_eq!(count_prefix(list.iter(), |_| false), 0);
+    assert_eq!(count_suffix(list.iter(), |_| false), 0);
   }
 }
