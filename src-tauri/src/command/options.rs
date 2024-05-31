70d61e32aa58b0d6ba5550b3ad92e712b5d17f3d
Silvio Mayolo <mercerenies@comcast.net>
2024-05-31 15:06:29 -0400

    keep_modifier in CommandOptions

--- src-tauri/src/command/options.rs
+++ src-tauri/src/command/options.rs
@@ -1,7 +1,15 @@
 
+/// Options passed in addition to a command.
 #[derive(Default, Clone, Debug, PartialEq, Eq)]
 pub struct CommandOptions {
+  /// The optional numerical argument for the command. This often
+  /// indicates where on the stack to apply the command, or to how
+  /// many elements.
   pub argument: Option<i64>,
+  /// The "keep" modifier, which indicates that the command should
+  /// preserve any stack elements that it utilizes, rather than
+  /// popping them.
+  pub keep_modifier: bool,
 }
 
 impl CommandOptions {
@@ -11,6 +19,11 @@ impl CommandOptions {
 
   pub fn with_argument(mut self, argument: i64) -> Self {
     self.argument = Some(argument);
+    self
+  }
+
+  pub fn with_keep_modifier(mut self) -> Self {
+    self.keep_modifier = true;
     self
   }
 }
