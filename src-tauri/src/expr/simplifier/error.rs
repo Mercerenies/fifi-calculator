6c1c7a1b7e654e4baa682c6000bdbf97465e90db
Silvio Mayolo <mercerenies@comcast.net>
2024-06-10 15:29:58 -0400

    SimplifierError is now an anyhow::Error under the hood
    SimplifierError is a struct containing anyhow::Error as well as the
    function that caused the error

--- src-tauri/src/expr/simplifier/error.rs
+++ src-tauri/src/expr/simplifier/error.rs
@@ -1,17 +1,35 @@
 
+//! Common errors that can occur during simplification.
+
 use thiserror::Error;
 
-// TODO: We might decide to convert this to anyhow::Error as our needs
-// grow with more simplifier types.
+use std::error::{Error as StdError};
+
+use std::fmt::{self, Display, Formatter};
 
 /// An error that occurred during the simplification process.
+/// Simplifier errors consist of the function that caused the error,
+/// as well as an arbitrary [`Error`](std::error::Error) object.
+#[derive(Debug)]
+pub struct SimplifierError {
+  function: String,
+  error: anyhow::Error,
+}
+
+/// Error indicating that the function's arity was not correct.
 #[derive(Debug, Clone, Error)]
-#[non_exhaustive]
-pub enum SimplifierError {
-  #[error("{function}: Expected {expected} argument(s) but got {actual}.")]
-  ArityError { function: String, expected: usize, actual: usize },
-  #[error("{function}: Domain error: {explanation}")]
-  DomainError { function: String, explanation: String },
+#[error("Expected {expected} argument(s) but got {actual}.")]
+pub struct ArityError {
+  pub expected: usize,
+  pub actual: usize,
+}
+
+/// Error indicating that one or more of the arguments for a function
+/// was out of the supported domain of the function.
+#[derive(Debug, Clone, Error)]
+#[error("Domain error: {explanation}")]
+pub struct DomainError {
+  pub explanation: String,
 }
 
 pub const DIVISION_BY_ZERO: &str = "Division by zero";
@@ -21,24 +39,40 @@ pub const EXPECTED_REAL: &str = "Expected real number";
 pub const ZERO_TO_ZERO_POWER: &str = "Indeterminate form 0^0";
 
 impl SimplifierError {
-  pub fn division_by_zero(function: impl Into<String>) -> SimplifierError {
-    SimplifierError::DomainError {
+  pub fn new<E, S>(function: S, error: E) -> Self
+  where S: Into<String>,
+        E: StdError + Send + Sync + 'static {
+    Self {
       function: function.into(),
-      explanation: DIVISION_BY_ZERO.to_owned(),
+      error: error.into(),
     }
+  }
+
+  pub fn function(&self) -> &str {
+    &self.function
+  }
+
+  pub fn division_by_zero(function: impl Into<String>) -> SimplifierError {
+    SimplifierError::new(function, DomainError { explanation: DIVISION_BY_ZERO.to_owned() })
   }
 
   pub fn expected_real(function: impl Into<String>) -> SimplifierError {
-    SimplifierError::DomainError {
-      function: function.into(),
-      explanation: EXPECTED_REAL.to_owned(),
-    }
+    SimplifierError::new(function, DomainError { explanation: EXPECTED_REAL.to_owned() })
   }
 
   pub fn zero_to_zero_power(function: impl Into<String>) -> SimplifierError {
-    SimplifierError::DomainError {
-      function: function.into(),
-      explanation: ZERO_TO_ZERO_POWER.to_owned(),
-    }
+    SimplifierError::new(function, DomainError { explanation: ZERO_TO_ZERO_POWER.to_owned() })
+  }
+}
+
+impl Display for SimplifierError {
+  fn fmt(&self, f: &mut Formatter) -> fmt::Result {
+    write!(f, "{}: {}", self.function, self.error)
+  }
+}
+
+impl StdError for SimplifierError {
+  fn source(&self) -> Option<&(dyn StdError + 'static)> {
+    Some(self.error.as_ref())
   }
 }
