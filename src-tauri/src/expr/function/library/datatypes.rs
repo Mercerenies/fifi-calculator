8f36039eba66b5ef14558d9ec74878073732e023
Silvio Mayolo <mercerenies@comcast.net>
2024-06-11 14:44:18 -0400

    Recognition of the vector() function

---
+++ src-tauri/src/expr/function/library/datatypes.rs
@@ -0,0 +1,20 @@
+
+//! Functions that represent concrete data in the language. These
+//! functions will usually not simplify, but they might have some
+//! useful properties we can exploit.
+
+use crate::expr::function::Function;
+use crate::expr::function::builder::{self, FunctionBuilder};
+use crate::expr::function::table::FunctionTable;
+
+pub fn append_datatype_functions(table: &mut FunctionTable) {
+  table.insert(vector_function());
+}
+
+pub fn vector_function() -> Function {
+  // Note: This function doesn't currently have any properties, but we
+  // still recognize it.
+  FunctionBuilder::new("vector")
+    .build()
+  // TODO: Derivative should be pointwise :)
+}
--- src-tauri/src/expr/function/library/mod.rs
+++ src-tauri/src/expr/function/library/mod.rs
@@ -9,13 +9,15 @@ use crate::expr::function::table::FunctionTable;
 mod arithmetic;
 mod basic;
 mod calculus;
+mod datatypes;
 mod transcendental;
 
 pub fn build_function_table() -> FunctionTable {
   let mut table = FunctionTable::new();
-  basic::append_basic_functions(&mut table);
   arithmetic::append_arithmetic_functions(&mut table);
-  transcendental::append_transcendental_functions(&mut table);
+  basic::append_basic_functions(&mut table);
   calculus::append_calculus_functions(&mut table);
+  datatypes::append_datatype_functions(&mut table);
+  transcendental::append_transcendental_functions(&mut table);
   table
 }
