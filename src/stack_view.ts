df2190e0212d6fecab21255e5ce31e03fa76fdfb
Silvio Mayolo <mercerenies@comcast.net>
2024-06-22 16:19:26 -0400

    Touch mode for dragging

--- src/stack_view.ts
+++ src/stack_view.ts
@@ -1,14 +1,7 @@
 
-import { TAURI } from './tauri_api.js';
-import { defaultCommandOptions } from './button_grid/modifier_delegate.js';
-
-import { Sortable, SortableStopEvent } from '@shopify/draggable';
-
 // Manager class for displaying the current value stack.
 export class StackView {
   private valueStackDiv: HTMLElement;
-  private sortable: Sortable | undefined;
-  private currentStackSize: number = 0;
   private delegate: StackUpdatedDelegate;
 
   constructor(valueStackDiv: HTMLElement, delegate?: StackUpdatedDelegate) {
@@ -17,13 +10,7 @@ export class StackView {
   }
 
   async refreshStack(newStackHtml: string[]): Promise<void> {
-    if (this.sortable) {
-      this.sortable.destroy();
-      this.sortable = undefined;
-    }
-
-    this.currentStackSize = newStackHtml.length;
-
+    this.valueStackDiv.dataset.stackLength = String(newStackHtml.length);
     const ol = document.createElement("ol");
     for (let i = 0; i < newStackHtml.length; i++) {
       const elem = newStackHtml[i];
@@ -45,30 +32,10 @@ export class StackView {
     stack.innerHTML = "";
     stack.appendChild(ol);
     await this.delegate.onStackUpdated(stack);
-
-    this.sortable = new Sortable(ol, {
-      draggable: 'li',
-    });
-    this.sortable.on('sortable:stop', (event) => this.onSortOrderUpdate(event));
   }
 
   scrollToBottom(): void {
     this.valueStackDiv.scrollTo({ top: this.valueStackDiv.scrollHeight });
-  }
-
-  private onSortOrderUpdate(event: SortableStopEvent): void {
-    const { oldIndex, newIndex } = event;
-    if ((oldIndex === undefined) || (newIndex === undefined)) {
-      throw `Indices are undefined, got {oldIndex: ${oldIndex}, newIndex: ${newIndex}}`;
-    }
-    // For the stack shuffle command, we count from the top of the
-    // stack, which is visually the bottom of the stack view.
-    const srcIndex = this.currentStackSize - 1 - oldIndex;
-    const destIndex = this.currentStackSize - 1 - newIndex;
-
-    window.setTimeout(() => {
-      TAURI.runMathCommand("move_stack_elem", [String(srcIndex), String(destIndex)], defaultCommandOptions());
-    }, 1);
   }
 }
 
--- src/touch_mode.ts
+++ src/touch_mode.ts
@@ -1,12 +1,16 @@
 
 import { StackUpdatedDelegate } from './stack_view.js';
+import { TAURI } from './tauri_api.js';
+import { defaultCommandOptions } from './button_grid/modifier_delegate.js';
+
+import { Sortable, SortableStopEvent } from '@shopify/draggable';
 
 // Manager for the "Touch Mode" radiobuttons which control what
 // happens when you click/drag a stack element.
 export class TouchModeManager implements StackUpdatedDelegate {
   private radiobuttonsDiv: HTMLElement;
   private valueStackDiv: HTMLElement;
-  private touchMode: TouchMode = TouchMode.DRAG;
+  private touchMode: TouchMode = NULL_TOUCH_MODE;
 
   constructor(args: TouchModeManagerArgs) {
     this.radiobuttonsDiv = args.radiobuttonsDiv;
@@ -19,19 +23,26 @@ export class TouchModeManager implements StackUpdatedDelegate {
       if (touchModeData == undefined) {
         throw "No touchMode data on radio button";
       }
-      const touchMode = TouchMode[touchModeData as keyof typeof TouchMode];
-      if (touchMode == undefined) {
+      const touchModeFactory = TouchModeFactories[touchModeData];
+      if (touchModeFactory == undefined) {
         throw "Invalid touchMode data on radio button";
       }
       inputElem.addEventListener('change', () => {
-        this.touchMode = touchMode;
+        this.touchMode.uninitTouchMode();
+        this.touchMode = touchModeFactory(this.valueStackDiv);
         this.updateTouchMode();
       });
+      if ((inputElem as HTMLInputElement).checked) {
+        this.touchMode = touchModeFactory(this.valueStackDiv);
+        // NOTE: Don't call updateTouchMode, as the HTML may not have
+        // been fully set up by this point. The first stack update
+        // will trigger the init call.
+      }
     }
   }
 
   private updateTouchMode(): void {
-    console.log(TouchMode[this.touchMode]);
+    this.touchMode.initTouchMode();
   }
 
   async onStackUpdated(): Promise<void> {
@@ -44,8 +55,64 @@ export interface TouchModeManagerArgs {
   valueStackDiv: HTMLElement;
 }
 
-export enum TouchMode {
-  DRAG,
-  VIEW,
-  EDIT,
+export const TouchModeFactories: Record<string, (elem: HTMLElement) => TouchMode> = {
+  DRAG: (elem) => new DragTouchMode(elem),
+  VIEW: () => NULL_TOUCH_MODE, // TODO
+  EDIT: () => NULL_TOUCH_MODE, // TODO
+};
+
+export interface TouchMode {
+  initTouchMode(): void;
+  uninitTouchMode(): void;
+}
+
+const NULL_TOUCH_MODE: TouchMode = {
+  initTouchMode() {},
+  uninitTouchMode() {},
+}
+
+export class DragTouchMode implements TouchMode {
+  private valueStackDiv: HTMLElement;
+  private sortable: Sortable | undefined;
+
+  constructor(valueStackDiv: HTMLElement) {
+    this.valueStackDiv = valueStackDiv;
+  }
+
+  initTouchMode(): void {
+    this.uninitTouchMode(); // un-init any existing touch mode
+
+    const orderedList = this.valueStackDiv.querySelector('ol');
+    if (!orderedList) {
+      console.warn("Could not find <ol> in value stack div");
+      return;
+    }
+    this.sortable = new Sortable(orderedList, {
+      draggable: 'li',
+    });
+    this.sortable.on('sortable:stop', (event) => this.onSortOrderUpdate(event));
+  }
+
+  uninitTouchMode(): void {
+    if (this.sortable) {
+      this.sortable.destroy();
+      this.sortable = undefined;
+    }
+  }
+
+  private onSortOrderUpdate(event: SortableStopEvent): void {
+    const { oldIndex, newIndex } = event;
+    if ((oldIndex === undefined) || (newIndex === undefined)) {
+      throw `Indices are undefined, got {oldIndex: ${oldIndex}, newIndex: ${newIndex}}`;
+    }
+    // For the stack shuffle command, we count from the top of the
+    // stack, which is visually the bottom of the stack view.
+    const currentStackSize = Number(this.valueStackDiv.dataset.stackLength);
+    const srcIndex = currentStackSize - 1 - oldIndex;
+    const destIndex = currentStackSize - 1 - newIndex;
+
+    window.setTimeout(() => {
+      TAURI.runMathCommand("move_stack_elem", [String(srcIndex), String(destIndex)], defaultCommandOptions());
+    }, 1);
+  }
 }
