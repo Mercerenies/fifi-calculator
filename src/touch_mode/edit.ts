8a9ae197b1b357c726b3cbbf261a08e368a6c0e3
Silvio Mayolo <mercerenies@comcast.net>
2024-06-22 17:49:54 -0400

    CSS for active element

--- src/touch_mode/edit.ts
+++ src/touch_mode/edit.ts
@@ -1,12 +1,11 @@
 
-///// Make it look nice while being clicked on :)
-
 import { TAURI } from '../tauri_api.js';
 import { defaultCommandOptions } from '../button_grid/modifier_delegate.js';
 import { InputBoxManager } from '../input_box.js';
 import { TouchMode, TouchModeFactoryContext } from '../touch_mode.js';
 import { FreeformInputMethod } from '../input_box/freeform_input.js';
-import { ALGEBRAIC_INPUT_PROMPT } from '../input_box/algebraic_input.js';
+
+const EDIT_INPUT_PROMPT = "Edit:";
 
 export class EditTouchMode implements TouchMode {
   private valueStackDiv: HTMLElement;
@@ -47,7 +46,7 @@ async function editStackFrame(manager: InputBoxManager, index: number): Promise<
   }
 
   const currentStackValue = await TAURI.getEditableStackElem(index);
-  const text = await manager.show(new FreeformInputMethod(ALGEBRAIC_INPUT_PROMPT), currentStackValue);
+  const text = await manager.show(new FreeformInputMethod(EDIT_INPUT_PROMPT), currentStackValue);
   if (text) {
     await TAURI.runMathCommand("replace_stack_elem", [String(index), text], defaultCommandOptions());
   }
