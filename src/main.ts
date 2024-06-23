f9604224f546a33c7fa098ae32a41b3586fe9db7
Silvio Mayolo <mercerenies@comcast.net>
2024-06-23 12:35:46 -0400

    UiManager class to its own file

--- src/main.ts
+++ src/main.ts
@@ -1,81 +1,10 @@
 
 import * as Page from './page.js';
-import { InputBoxManager } from './input_box.js';
-import { NotificationManager } from './notifications.js';
-import { MainButtonGrid } from './button_grid/main_button_grid.js';
+import { UiManager } from './ui_manager.js';
 import { defaultCommandOptions } from './button_grid/modifier_delegate.js';
-import { KeyInput } from './keyboard.js';
-import * as KeyDispatcher from './keyboard/dispatcher.js';
-import { RightPanelManager } from './right_panel.js';
 import { TAURI, RefreshStackPayload, UndoAvailabilityPayload } from './tauri_api.js';
 import { StackView, StackUpdatedDelegate } from './stack_view.js';
 import { GraphicsEngine } from './graphics_engine.js';
-
-import { OsType } from '@tauri-apps/plugin-os';
-
-class UiManager {
-  readonly inputManager: InputBoxManager;
-  readonly notificationManager: NotificationManager;
-  readonly rightPanelManager: RightPanelManager;
-  readonly osType: OsType;
-
-  private keyHandler: KeyDispatcher.KeyEventHandler;
-  private keyEventListener: (event: KeyboardEvent) => Promise<void>;
-
-  constructor(osType: OsType) {
-    this.inputManager = new InputBoxManager({
-      inputBox: Page.getInputBoxDiv(),
-      inputTextBox: Page.getInputTextBox(),
-      inputLabel: Page.getInputTextBoxLabel(),
-    });
-    this.notificationManager = new NotificationManager(Page.getNotificationBox());
-    this.rightPanelManager = new RightPanelManager({
-      buttonGrid: Page.getButtonGridContainer(),
-      prefixPanel: Page.getPrefixArgPanel(),
-      keepModifierCheckbox: Page.getModifierArgKeepArgCheckbox(),
-      initialGrid: new MainButtonGrid(this.inputManager, this.notificationManager),
-      undoButton: Page.getUndoButton(),
-      redoButton: Page.getRedoButton(),
-      radiobuttonsDiv: Page.getTouchModesDiv(),
-      valueStackDiv: Page.getValueStack(),
-      inputManager: this.inputManager,
-    });
-    this.osType = osType;
-
-    this.keyHandler = KeyDispatcher.sequential([
-      KeyDispatcher.filtered(
-        this.inputManager,
-        () => document.activeElement === Page.getInputTextBox(),
-      ),
-      this.rightPanelManager.undoManager,
-      this.rightPanelManager.buttonGrid,
-    ]);
-    this.keyEventListener = (event) => this.dispatchOnKey(event);
-  }
-
-  static async create(): Promise<UiManager> {
-    const osType = await TAURI.osType();
-    return new UiManager(osType);
-  }
-
-  initListeners(): void {
-    this.inputManager.initListeners();
-    this.notificationManager.initListeners();
-    this.rightPanelManager.initListeners();
-    document.body.addEventListener("keydown", this.keyEventListener);
-  }
-
-  private async dispatchOnKey(event: KeyboardEvent): Promise<void> {
-    const input = KeyInput.fromEvent(event, this.osType);
-    if (input === undefined) {
-      // The pressed key was a modifier key like Ctrl or Alt, so
-      // ignore it.
-      return;
-    }
-
-    this.keyHandler.onKeyDown(input);
-  }
-}
 
 async function refreshStack(stackView: StackView, payload: RefreshStackPayload): Promise<void> {
   await stackView.refreshStack(payload.stack);
---
+++ src/ui_manager.ts
@@ -0,0 +1,75 @@
+
+import { InputBoxManager } from './input_box.js';
+import { NotificationManager } from './notifications.js';
+import { MainButtonGrid } from './button_grid/main_button_grid.js';
+import { KeyInput } from './keyboard.js';
+import * as KeyDispatcher from './keyboard/dispatcher.js';
+import { RightPanelManager } from './right_panel.js';
+import * as Page from './page.js';
+import { TAURI } from './tauri_api.js';
+
+import { OsType } from '@tauri-apps/plugin-os';
+
+export class UiManager {
+  readonly inputManager: InputBoxManager;
+  readonly notificationManager: NotificationManager;
+  readonly rightPanelManager: RightPanelManager;
+  readonly osType: OsType;
+
+  private keyHandler: KeyDispatcher.KeyEventHandler;
+  private keyEventListener: (event: KeyboardEvent) => Promise<void>;
+
+  constructor(osType: OsType) {
+    this.inputManager = new InputBoxManager({
+      inputBox: Page.getInputBoxDiv(),
+      inputTextBox: Page.getInputTextBox(),
+      inputLabel: Page.getInputTextBoxLabel(),
+    });
+    this.notificationManager = new NotificationManager(Page.getNotificationBox());
+    this.rightPanelManager = new RightPanelManager({
+      buttonGrid: Page.getButtonGridContainer(),
+      prefixPanel: Page.getPrefixArgPanel(),
+      keepModifierCheckbox: Page.getModifierArgKeepArgCheckbox(),
+      initialGrid: new MainButtonGrid(this.inputManager, this.notificationManager),
+      undoButton: Page.getUndoButton(),
+      redoButton: Page.getRedoButton(),
+      radiobuttonsDiv: Page.getTouchModesDiv(),
+      valueStackDiv: Page.getValueStack(),
+      inputManager: this.inputManager,
+    });
+    this.osType = osType;
+
+    this.keyHandler = KeyDispatcher.sequential([
+      KeyDispatcher.filtered(
+        this.inputManager,
+        () => document.activeElement === Page.getInputTextBox(),
+      ),
+      this.rightPanelManager.undoManager,
+      this.rightPanelManager.buttonGrid,
+    ]);
+    this.keyEventListener = (event) => this.dispatchOnKey(event);
+  }
+
+  static async create(): Promise<UiManager> {
+    const osType = await TAURI.osType();
+    return new UiManager(osType);
+  }
+
+  initListeners(): void {
+    this.inputManager.initListeners();
+    this.notificationManager.initListeners();
+    this.rightPanelManager.initListeners();
+    document.body.addEventListener("keydown", this.keyEventListener);
+  }
+
+  private async dispatchOnKey(event: KeyboardEvent): Promise<void> {
+    const input = KeyInput.fromEvent(event, this.osType);
+    if (input === undefined) {
+      // The pressed key was a modifier key like Ctrl or Alt, so
+      // ignore it.
+      return;
+    }
+
+    this.keyHandler.onKeyDown(input);
+  }
+}
