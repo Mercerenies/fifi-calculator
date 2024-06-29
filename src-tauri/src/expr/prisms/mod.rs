a0c4a353d22a3769458661e5b4f0ca87805c1a4c
Silvio Mayolo <mercerenies@comcast.net>
2024-06-29 11:58:13 -0400

    expr_to_string prism

--- src-tauri/src/expr/prisms/mod.rs
+++ src-tauri/src/expr/prisms/mod.rs
@@ -89,6 +89,11 @@ pub fn expr_to_number() -> impl Prism<Expr, Number> + Clone {
   Conversion::new()
 }
 
+/// Prism which only accepts string literals.
+pub fn expr_to_string() -> impl Prism<Expr, String> + Clone {
+  Conversion::new()
+}
+
 /// Prism which accepts only positive real numbers.
 pub fn expr_to_positive_number() -> impl Prism<Expr, PositiveNumber> + Clone {
   expr_to_number().composed(NumberToPositiveNumber)
