cae939a4115f75a42b47d1f00f00245794b425b6
Silvio Mayolo <mercerenies@comcast.net>
2024-07-07 18:39:42 -0400

    Replace ValidationError with anyhow::Error

--- src-tauri/src/state/validation.rs
+++ src-tauri/src/state/validation.rs
@@ -2,12 +2,11 @@
 //! Helpers for validating text from the frontend against various
 //! conditions.
 
+use anyhow::Context;
+
 use crate::expr::var::Var;
 
 use serde::{Serialize, Deserialize};
-
-use std::fmt::{self, Display, Formatter};
-use std::error::{Error as StdError};
 
 /// Types of validations that can be requested of the backend.
 #[derive(Debug, Copy, Clone, Serialize, Deserialize)]
@@ -18,40 +17,15 @@ pub enum Validator {
   Variable,
 }
 
-#[derive(Debug)]
-pub struct ValidationError {
-  inner: Box<dyn StdError + Send + Sync + 'static>
-}
-
-impl ValidationError {
-  pub fn from_error(err: impl StdError + Send + Sync + 'static) -> Self {
-    Self {
-      inner: Box::new(err),
-    }
-  }
-}
-
-impl Display for ValidationError {
-  fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
-    self.inner.fmt(f)
-  }
-}
-
-impl StdError for ValidationError {
-  fn source(&self) -> Option<&(dyn StdError + 'static)> {
-    Some(&*self.inner)
-  }
-}
-
-pub fn validate(validator: Validator, payload: String) -> Result<(), ValidationError> {
+pub fn validate(validator: Validator, payload: String) -> anyhow::Result<()> {
   match validator {
     Validator::Variable => validate_var(payload).map(|_| ())
   }
 }
 
 /// Validates that the given string is a valid variable name.
-pub fn validate_var(name: String) -> Result<Var, ValidationError> {
-  Var::try_from(name).map_err(ValidationError::from_error)
+pub fn validate_var(name: String) -> Result<Var, anyhow::Error> {
+  Var::try_from(name).context("Validation failed: invalid variable name")
 }
 
 #[cfg(test)]
