c48ccfa97454f220d4eddc17148ba274438b6e78
Silvio Mayolo <mercerenies@comcast.net>
2024-07-30 15:09:35 -0400

    Column and ColumnMut borrowing structs

--- src-tauri/src/util/matrix.rs
+++ src-tauri/src/util/matrix.rs
@@ -9,6 +9,7 @@ use serde::{Serialize, Deserialize};
 
 use std::fmt::Debug;
 use std::ops::{Index, IndexMut};
+use std::borrow::ToOwned;
 
 /// A `Matrix<T>` is a vector of vectors of `T` in which each
 /// constituent vector has the same length.
@@ -16,6 +17,22 @@ use std::ops::{Index, IndexMut};
 #[serde(try_from = "Vec<Vec<T>>")]
 pub struct Matrix<T> {
   body: Vec<Vec<T>>,
+}
+
+/// A (borrowed) column of a [`Matrix`]. It can be assumed that a
+/// [`Column`] is always in-bounds.
+#[derive(Debug, Clone)]
+pub struct Column<'a, T> {
+  matrix: &'a Matrix<T>,
+  column_index: usize,
+}
+
+/// A column of a [`Matrix`], borrowed mutably. It can be assumed that
+/// a [`ColumnMut`] is always in-bounds.
+#[derive(Debug)]
+pub struct ColumnMut<'a, T> {
+  matrix: &'a mut Matrix<T>,
+  column_index: usize,
 }
 
 /// An index into a matrix. Matrix indices are 0-based, like all Rust
@@ -77,6 +94,26 @@ impl<T> Matrix<T> {
     self.body.get(index).map(|row| row.as_slice())
   }
 
+  pub fn row_mut(&mut self, index: usize) -> Option<&mut [T]> {
+    self.body.get_mut(index).map(|row| row.as_mut_slice())
+  }
+
+  pub fn column(&self, index: usize) -> Option<Column<'_, T>> {
+    if index < self.width() {
+      Some(Column { matrix: self, column_index: index })
+    } else {
+      None
+    }
+  }
+
+  pub fn column_mut(&mut self, index: usize) -> Option<ColumnMut<'_, T>> {
+    if index < self.width() {
+      Some(ColumnMut { matrix: self, column_index: index })
+    } else {
+      None
+    }
+  }
+
   pub fn get(&self, index: MatrixIndex) -> Option<&T> {
     self.body
       .get(index.y)
@@ -124,6 +161,52 @@ impl<T> Matrix<T> {
   }
 }
 
+impl<'a, T> Column<'a, T> {
+  pub fn get(&self, index: usize) -> Option<&T> {
+    self.matrix.get(MatrixIndex { y: index, x: self.column_index })
+  }
+
+  pub fn len(&self) -> usize {
+    self.matrix.height()
+  }
+
+  pub fn iter(&self) -> impl Iterator<Item = &T> + '_ {
+    self.matrix.body.iter().map(|row| &row[self.column_index])
+  }
+
+  pub fn to_owned(&self) -> Vec<T::Owned>
+  where T: ToOwned {
+    self.iter().map(T::to_owned).collect()
+  }
+}
+
+impl<'a, T> ColumnMut<'a, T> {
+  pub fn get(&self, index: usize) -> Option<&T> {
+    self.matrix.get(MatrixIndex { y: index, x: self.column_index })
+  }
+
+  pub fn get_mut(&mut self, index: usize) -> Option<&mut T> {
+    self.matrix.get_mut(MatrixIndex { y: index, x: self.column_index })
+  }
+
+  pub fn len(&self) -> usize {
+    self.matrix.height()
+  }
+
+  pub fn iter(&self) -> impl Iterator<Item = &T> + '_ {
+    self.matrix.body.iter().map(|row| &row[self.column_index])
+  }
+
+  pub fn iter_mut(&mut self) -> impl Iterator<Item = &mut T> + '_ {
+    self.matrix.body.iter_mut().map(|row| &mut row[self.column_index])
+  }
+
+  pub fn to_owned(&self) -> Vec<T::Owned>
+  where T: ToOwned {
+    self.iter().map(T::to_owned).collect()
+  }
+}
+
 impl<T> Default for Matrix<T> {
   fn default() -> Self {
     Matrix::empty()
@@ -160,6 +243,28 @@ impl<T> IndexMut<MatrixIndex> for Matrix<T> {
   }
 }
 
+impl<'a, T> Index<usize> for Column<'a, T> {
+  type Output = T;
+
+  fn index(&self, index: usize) -> &Self::Output {
+    &self.matrix[MatrixIndex { y: index, x: self.column_index }]
+  }
+}
+
+impl<'a, T> Index<usize> for ColumnMut<'a, T> {
+  type Output = T;
+
+  fn index(&self, index: usize) -> &Self::Output {
+    &self.matrix[MatrixIndex { y: index, x: self.column_index }]
+  }
+}
+
+impl<'a, T> IndexMut<usize> for ColumnMut<'a, T> {
+  fn index_mut(&mut self, index: usize) -> &mut Self::Output {
+    &mut self.matrix[MatrixIndex { y: index, x: self.column_index }]
+  }
+}
+
 impl<T: Serialize> Serialize for Matrix<T> {
   fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
   where S: serde::Serializer {
@@ -183,5 +288,42 @@ mod tests {
     let json = serde_json::to_string(&matrix).unwrap();
     let deserialized_matrix: Matrix<usize> = serde_json::from_str(&json).unwrap();
     assert_eq!(deserialized_matrix, matrix);
+  }
+
+  #[test]
+  fn test_row_accessor() {
+    let matrix = Matrix::new(vec![
+      vec![1, 2, 3],
+      vec![4, 5, 6],
+      vec![7, 8, 9],
+    ]).unwrap();
+    assert_eq!(&matrix[0], [1, 2, 3].as_slice());
+    assert_eq!(&matrix[1], [4, 5, 6].as_slice());
+    assert_eq!(&matrix[2], [7, 8, 9].as_slice());
+    assert_eq!(matrix.row(0), Some([1, 2, 3].as_slice()));
+    assert_eq!(matrix.row(1), Some([4, 5, 6].as_slice()));
+    assert_eq!(matrix.row(2), Some([7, 8, 9].as_slice()));
+    assert_eq!(matrix.row(3), None);
+  }
+
+  #[test]
+  fn test_column_accessor() {
+    let mut matrix = Matrix::new(vec![
+      vec![1, 2, 3],
+      vec![4, 5, 6],
+      vec![7, 8, 9],
+    ]).unwrap();
+    assert_eq!(matrix.column(0).unwrap().to_owned(), vec![1, 4, 7]);
+    assert_eq!(matrix.column(1).unwrap().to_owned(), vec![2, 5, 8]);
+    assert_eq!(matrix.column(2).unwrap().to_owned(), vec![3, 6, 9]);
+    assert!(matrix.column(3).is_none());
+
+    let mut col = matrix.column_mut(1).unwrap();
+    col[2] = 99;
+    assert_eq!(matrix.into_row_major(), vec![
+      vec![1, 2, 3],
+      vec![4, 5, 6],
+      vec![7, 99, 9],
+    ]);
   }
 }
