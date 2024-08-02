d1ac6bbf6e7abc17c1549ed064804f7bbd234225
Silvio Mayolo <mercerenies@comcast.net>
2024-08-01 21:21:43 -0400

    add some traits where needed for MatrixElement

--- src-tauri/src/util/matrix/base.rs
+++ src-tauri/src/util/matrix/base.rs
@@ -1,7 +1,9 @@
 
 use crate::util::Recip;
 
-use std::ops::{Add, Mul};
+use num::One;
+
+use std::ops::{Add, Mul, Neg};
 
 /// Trait defining elements that are arithmetic enough to be used in
 /// matrix operations.
@@ -12,7 +14,7 @@ use std::ops::{Add, Mul};
 ///
 /// Structs needn't implement this trait by hand, as a blanket impl
 /// takes care of it for any satisfactory type.
-pub trait MatrixElement: Clone + for<'a> Add<&'a Self, Output=Self> + for<'a> Mul<&'a Self, Output=Self> {}
+pub trait MatrixElement: Clone + One + for<'a> Add<&'a Self, Output=Self> + for<'a> Mul<&'a Self, Output=Self> + Neg<Output=Self> {}
 
 /// Trait for elements that are both [`MatrixElement`] and [`Recip`].
 /// This is the trait required to be able to fully row reduce a
@@ -21,6 +23,6 @@ pub trait MatrixElement: Clone + for<'a> Add<&'a Self, Output=Self> + for<'a> Mu
 /// implementations are not necessary.
 pub trait MatrixFieldElement: MatrixElement + Recip<Output=Self> {}
 
-impl<T> MatrixElement for T where T: Clone + for<'a> Add<&'a T, Output=T> + for<'a> Mul<&'a T, Output=T> {}
+impl<T> MatrixElement for T where T: Clone + One + for<'a> Add<&'a Self, Output=Self> + for<'a> Mul<&'a Self, Output=Self> + Neg<Output=Self> {}
 
 impl<T> MatrixFieldElement for T where T: MatrixElement + Recip<Output=T> {}
