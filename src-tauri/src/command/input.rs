587e474783db62cb47ae3732d54f6f67dfd1f8f4
Silvio Mayolo <mercerenies@comcast.net>
2024-08-09 20:30:50 -0400

    Algebraic push command supports $ vars too

--- src-tauri/src/command/input.rs
+++ src-tauri/src/command/input.rs
@@ -7,8 +7,11 @@ use super::arguments::{UnaryArgumentSchema, validate_schema};
 use super::options::CommandOptions;
 use super::subcommand::Subcommand;
 use crate::util::prism::Identity;
+use crate::util::cow_dyn::CowDyn;
 use crate::expr::Expr;
 use crate::expr::number::Number;
+use crate::expr::simplifier::Simplifier;
+use crate::expr::simplifier::dollar_sign::DollarSignRefSimplifier;
 use crate::state::ApplicationState;
 use crate::stack::base::StackLike;
 use crate::mode::display::language::LanguageMode;
@@ -20,14 +23,30 @@ use std::str::FromStr;
 /// is done via a custom function supplied by the constructor.
 ///
 /// On parse failure, the stack is not modified.
-pub struct PushInputCommand<F> {
+pub struct PushInputCommand<F, SF> {
   body: F,
+  simplifier_function: SF,
 }
 
-impl<F> PushInputCommand<F>
+impl<F> PushInputCommand<F, for<'s> fn(&'s (dyn Simplifier + 's), &'s ApplicationState) -> CowDyn<'s, dyn Simplifier + 's>>
 where F: Fn(String, &dyn LanguageMode) -> anyhow::Result<Expr> {
   pub fn new(body: F) -> Self {
-    Self { body }
+    Self {
+      body,
+      simplifier_function: |simpl, _| CowDyn::Borrowed(simpl),
+    }
+  }
+}
+
+impl<F, SF> PushInputCommand<F, SF>
+where F: Fn(String, &dyn LanguageMode) -> anyhow::Result<Expr>,
+      SF: for<'s> Fn(&'s (dyn Simplifier + 's), &'s ApplicationState) -> CowDyn<'s, dyn Simplifier + 's> {
+  pub fn with_simplifier<SF1>(self, simplifier_function: SF1) -> PushInputCommand<F, SF1>
+  where SF1: for<'s> Fn(&'s (dyn Simplifier + 's), &'s ApplicationState) -> CowDyn<'s, dyn Simplifier + 's> {
+    PushInputCommand {
+      body: self.body,
+      simplifier_function,
+    }
   }
 
   pub fn try_parse(&self, arg: String, language_mode: &dyn LanguageMode) -> anyhow::Result<Expr> {
@@ -35,8 +54,9 @@ where F: Fn(String, &dyn LanguageMode) -> anyhow::Result<Expr> {
   }
 }
 
-impl<F> Command for PushInputCommand<F>
-where F: Fn(String, &dyn LanguageMode) -> anyhow::Result<Expr> {
+impl<F, SF> Command for PushInputCommand<F, SF>
+where F: Fn(String, &dyn LanguageMode) -> anyhow::Result<Expr>,
+      SF: for<'s> Fn(&'s (dyn Simplifier + 's), &'s ApplicationState) -> CowDyn<'s, dyn Simplifier + 's> {
   fn run_command(
     &self,
     state: &mut ApplicationState,
@@ -49,7 +69,9 @@ where F: Fn(String, &dyn LanguageMode) -> anyhow::Result<Expr> {
 
     state.undo_stack_mut().push_cut();
     let expr = self.try_parse(arg, state.display_settings().language_mode().as_ref())?;
-    let expr = context.simplify_expr(expr, calculation_mode, &mut errors);
+    let expr = context.simplify_expr_using(expr, calculation_mode, &mut errors, |base_simplifier| {
+      (self.simplifier_function)(base_simplifier, state)
+    });
     state.main_stack_mut().push(expr);
     Ok(CommandOutput::from_errors(errors))
   }
@@ -65,7 +87,7 @@ fn argument_schema() -> UnaryArgumentSchema<Identity, String> {
 
 /// A `PushInputCommand` which parses a literal real number and pushes
 /// it onto the stack.
-pub fn push_number_command() -> PushInputCommand<impl Fn(String, &dyn LanguageMode) -> anyhow::Result<Expr>> {
+pub fn push_number_command() -> impl Command {
   PushInputCommand::new(|arg, _| {
     let number = Number::from_str(&arg)?;
     Ok(Expr::from(number))
@@ -74,13 +96,15 @@ pub fn push_number_command() -> PushInputCommand<impl Fn(String, &dyn LanguageMo
 
 /// A `PushInputCommand` which uses the current language mode to parse
 /// a general expression.
-pub fn push_expr_command() -> PushInputCommand<impl Fn(String, &dyn LanguageMode) -> anyhow::Result<Expr>> {
-  PushInputCommand::new(|arg, language_mode| language_mode.parse(&arg))
+pub fn push_expr_command() -> impl Command {
+  PushInputCommand::new(|arg, language_mode| language_mode.parse(&arg)).with_simplifier(|base_simplifier, state| {
+    CowDyn::Owned(Box::new(DollarSignRefSimplifier::prepended(state.main_stack(), base_simplifier)))
+  })
 }
 
 /// A `PushInputCommand` which pushes a literal string onto the stack.
 /// Always succeeds.
-pub fn push_string_command() -> PushInputCommand<impl Fn(String, &dyn LanguageMode) -> anyhow::Result<Expr>> {
+pub fn push_string_command() -> impl Command {
   PushInputCommand::new(|arg, _| Ok(Expr::from(arg)))
 }
 
@@ -121,6 +145,22 @@ mod tests {
       input_stack,
     ).unwrap();
     assert_eq!(output_stack, stack_of(vec![10, 20, 30, 400]));
+  }
+
+  #[test]
+  fn test_push_expr_command_with_stack_references() {
+    let input_stack = vec![10, 20, 30];
+    let output_stack = act_on_stack(
+      &push_expr_command(),
+      vec!["$1 - $2"],
+      input_stack,
+    ).unwrap();
+    assert_eq!(output_stack, stack_of(vec![
+      Expr::from(10),
+      Expr::from(20),
+      Expr::from(30),
+      Expr::call("-", vec![Expr::from(30), Expr::from(20)]),
+    ]));
   }
 
   #[test]
