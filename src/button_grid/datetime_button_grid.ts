d7f09f4b32fe95982394a182a4c1aac02f833e01
Silvio Mayolo <mercerenies@comcast.net>
2025-05-16 20:58:55 -0400

    Starting template for DatetimeButtonGrid

---
+++ src/button_grid/datetime_button_grid.ts
@@ -0,0 +1,33 @@
+
+import { ButtonGrid, GridCell } from "../button_grid.js";
+import { backButton, DispatchButton } from './button.js';
+
+export class DatetimeButtonGrid extends ButtonGrid {
+  readonly rows: readonly (readonly GridCell[])[];
+
+  private rootGrid: ButtonGrid;
+
+  constructor(rootGrid: ButtonGrid) {
+    super();
+    this.rootGrid = rootGrid;
+    this.rows = this.initRows();
+  }
+
+  private initRows(): GridCell[][] {
+    return [
+      [
+      ],
+      [
+      ],
+      [
+      ],
+      [
+      ],
+      [
+      ],
+      [
+        backButton(this.rootGrid),
+      ],
+    ];
+  }
+}
