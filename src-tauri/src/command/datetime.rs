8af13477d8700e5cedcca43b07d2c8d2e59728c8
Mercerenies <mercerenies@gmail.com>
2026-01-10 17:48:03 -0500

    fix bug in datetime function

--- src-tauri/src/command/datetime.rs
+++ src-tauri/src/command/datetime.rs
@@ -57,9 +57,9 @@ pub const ZERO_DATE: DateTime =
 /// Day "zero" for the purposes of Julian day calculations. The Julian
 /// day of a date is defined as the number of days since Jan 1, 4713
 /// BC on the Julian calendar. Using the proleptic Julian calendar,
-/// this is Nov 24, 4713 BC.
+/// this is Nov 24, 4714 BC.
 pub const ZERO_JULIAN_DAY: DateTime =
-  DateTime::from_date(date!(-4713-11-24));
+  DateTime::from_date(date!(-4714-11-24));
 
 /// The Unix epoch.
 pub const UNIX_EPOCH: DateTime =
